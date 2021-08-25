2118fade73248aa83a2d0d543b382e1560e2cf22
Raven Dell <20336@my4county.net>
2021-08-25 09:19:46 -0400

    made a bunny

---
+++ src/asciiart/AsciiArt.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asciiart;
+
+/**
+ *
+ * @author rdell
+ */
+public class AsciiArt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("_       _ ");
+        System.out.println("\\ \\   / /");
+        System.out.println(" \\ \\_/ /");
+        System.out.println(" /O + O\\");
+        System.out.println(" \\_____/");
+        System.out.println(" /     \\");
+        System.out.println("/\\_\\ /_/\\");
+        System.out.println("\\   _   /");
+        System.out.println("/_ / \\ _\\");
+    }
+    
+}
